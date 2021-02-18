159466597dd0825d7e549e2045374ffd1e813a77
rockysec <48323046+rockysec@users.noreply.github.com>
2021-02-18 10:51:56 -0300

    Update loop_aquatone.sh

--- loop_aquatone.sh
+++ loop_aquatone.sh
@@ -1,9 +1,9 @@
 #/bin/bash
 input="$1"
 output="$2"
-while IFS= read -r line
-do
-# cat $input | aquatone -out $output -threads 5
-aquatone $line -out $output -threads 5
-sleep 3
-done <"${input}"
+#while IFS= read -r line
+#do
+cat $input | aquatone -out $output -threads 5
+#aquatone $line -out $output -threads 5
+#sleep 3
+#done <"${input}"
