88259af17a50b73839d19875b80486d92ef4354f
rockysec <48323046+rockysec@users.noreply.github.com>
2021-04-22 13:06:01 -0300

    Update loop_ffuf.sh

--- loop_ffuf.sh
+++ loop_ffuf.sh
@@ -2,6 +2,6 @@
 input="$1"
 while IFS= read -r line
 do
-ffuf -w /root/tools/dirsearch/db/dicc.txt -u $line/FUZZ -t 200 -fc 301,302,401,403,400 -e sql,txt,zip,jsp,log,logs,old,tar.gz,gz,tar,bkp,dump,db,php,xml,py,asp,aspx,rar,do,1,asmx,rar,key,gpg,asc,pl,json -recursion -recursion-depth 2
+ffuf -of csv -o ./ffuf.csv -w /mnt/volume_nyc3_01/tools/dirsearch/db/dicc.txt -u $line/FUZZ -t 200 -fc 301,302,401,403,400 -e sql,txt,zip,jsp,log,logs,old,tar.gz,gz,tar,bkp,dump,db,php,xml,py,asp,aspx,rar,do,1,asmx,rar,key,gpg,asc,pl,json -recursion -recursion-depth 2
 sleep 3
 done <"${input}"
