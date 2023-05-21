8f467d2fcd5a72bd4d611443434e6f3a8483bc25
rockysec <48323046+rockysec@users.noreply.github.com>
2023-05-21 13:06:10 -0500

    Update loop_linkfinder.sh

--- loop_linkfinder.sh
+++ loop_linkfinder.sh
@@ -2,6 +2,6 @@
 input="$1"
 while IFS= read -r line
 do
-python3 ~/tools/LinkFinder/linkfinder.py -i $line -o cli >> linkfinder.txt
+python3 ~/tools/LinkFinder/linkfinder.py -i $line -o cli
 sleep 3
 done <"${input}"
