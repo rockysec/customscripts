9c6be84b8faf8c2d6bacf6135dc7387c33142e06
rockysec <48323046+rockysec@users.noreply.github.com>
2023-08-14 17:32:59 -0500

    adding -pp flag

--- loop_scan.sh
+++ loop_scan.sh
@@ -2,6 +2,6 @@
 input="$1"
 while read -u 10 line;
 do
-bash ~/tools/RockySecTools/RockySecTool.sh -domain $line -recon -wayback -nuclei_cves -nuclei_dlogins -nuclei_panels -nuclei_exposures -nuclei_misc -nuclei_misconfig -nuclei_takeovers -nuclei_tech -nuclei_vuln -cors -nmap -xss -crlf -sqli -or -output
+bash ~/tools/RockySecTools/RockySecTool.sh -domain $line -recon -wayback -nuclei_cves -nuclei_dlogins -nuclei_panels -nuclei_exposures -nuclei_misc -nuclei_misconfig -nuclei_takeovers -nuclei_tech -nuclei_vuln -cors -nmap -xss -crlf -sqli -or -pp -output
 sleep 3
 done 10< "${1}"
